bcdfeb4ca152642e71480309be6450d96d70c7cf
Luciano Sabatino <luciano@lucianosabatino.com>
2013-07-31 20:06:27 +0200

    start project from tema italia modified

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  Theme Name: My Custom Theme
+  Theme URI: http://letoghe.net
+  Description: letoghe.net theme
+  Version: .0
+  Author: Me
+  Author URI: http://www.letoghe.net/
+  Widgets: header,footer
+  Theme update URI: 
+*/
+
+    function italia_theme_info() {
+        return array(
+            'name'        => 'OSClass Italia theme',
+            'version'     => '1.0.2',
+            'description' => 'This is the OSClass Italia theme',
+            'author_name' => 'OSClass team',
+            'author_url'  => 'http://osclass.org',
+            'locations'   => array('header', 'footer')
+        );
+    }
+
+?>
